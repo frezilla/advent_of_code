b23ae60e9664bc0fe0f7685400ddd77bec3d27a0
fr3zill@ <fb.info@mailo.com>
2025-11-17 23:02:56 +0100

    Enigme du jour 19 - sauvegarde des développements

--- 2015/19/main.py
+++ 2015/19/main.py
@@ -4,11 +4,17 @@
 
 
 def load_replacements():
+    replacements = dict()
     puzzle = open("puzzle.txt", "r")
     for line in puzzle:
         current_line = line.rstrip()
         if len(current_line) == 0:
             break
+        datas = current_line.split()
+        current_values = replacements.setdefault(datas[0], list())
+        current_values.append(datas[2])
+        replacements.update({datas[0]: current_values})
+    return replacements
 
 
 def run():
