c21e0da0214ca4f984eb2e4a45f4b8b149016946
fr3zill@ <fb.info@mailo.com>
2025-11-20 23:27:03 +0100

    Enigme du jour 19

--- 2015/19/main.py
+++ 2015/19/main.py
@@ -57,6 +57,25 @@ def run():
                 for new_molecule in new_molecules:
                     molecules.add(new_molecule)
     print(f"Nombre de molécules uniques qui peuvent être créées : {len(molecules)}")
+    replacement_reversed = dict()
+    for key in replacements.keys():
+        values = replacements.get(key)
+        for value in values:
+            replacement_reversed[value] = key
+    keys = sorted(replacement_reversed.keys(), key=len, reverse=True)
+    replacements_reversed_ordered = dict()
+    for key in keys:
+        replacements_reversed_ordered.update({key: replacement_reversed[key]})
+    nb_steps = 0
+    current_value = initial_value
+    while current_value != "e":
+        for key in replacements_reversed_ordered.keys():
+            nb_indexes = len(search_all_distincts_indexes_of(current_value, key))
+            if nb_indexes != 0:
+                nb_steps += nb_indexes
+                current_value = current_value.replace(key, replacements_reversed_ordered[key])
+                break
+    print(f"Nombre d'étapes pour générer la molécule cible : {nb_steps}")
 
 
 def search_all_indexes_of(str, value, start_index = 0):
@@ -67,6 +86,17 @@ def search_all_indexes_of(str, value, start_index = 0):
         if index + 1 < len(str):
             new_str = str[index + 1:]
             result = result + search_all_indexes_of(new_str, value, index + start_index + 1)
+    return result
+
+
+def search_all_distincts_indexes_of(str, value, start_index = 0):
+    result = list()
+    index = index_of(str, value)
+    if index != -1:
+        result.append(index + start_index)
+        if index + len(value) < len(str):
+            new_str = str[index + len(value):]
+            result = result + search_all_distincts_indexes_of(new_str, value, index + len(value))
     return result
 
 
